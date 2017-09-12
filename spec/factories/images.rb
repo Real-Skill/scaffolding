39a4615fbb236a7876c6f35455aee17e1cf988a4
MichalNiedziela1601 <michal.niedziela@labnoratory.com>
2017-09-12 20:57:41 +0200

    update rails scaffolding

--- spec/factories/images.rb
+++
@@ -1,7 +0,0 @@
-FactoryGirl.define do
-  factory :image do
-    sequence(:name) { |n| "Image #{n}" }
-    sequence(:url) { |n| "http://images.com/original#{n}.jpeg" }
-    description Faker::Lorem.sentence
-  end
-end
--- spec/factories/videos.rb
+++
@@ -1,7 +0,0 @@
-FactoryGirl.define do
-  factory :video do
-    sequence(:name) { |n| "Video #{n}" }
-    sequence(:url) { |n| "http://videos.com/original#{n}.mp4" }
-    description Faker::Lorem.sentence
-  end
-end
