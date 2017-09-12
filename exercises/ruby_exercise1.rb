b73a4d43e0abe4e631e8694707577b74b8c8b4ea
MichalNiedziela1601 <michal.niedziela@labnoratory.com>
2017-09-12 17:08:41 +0200

    update ruby scaffolding

--- exercises/ruby_exercise1.rb
+++ exercises/ruby_exercise1.rb
@@ -1,36 +1,14 @@
 module RubyExercise1
-  class UsersList
+  class SimpleNumber
 
-    NoUserError = Class.new(StandardError)
-
-    def initialize
-      @users = []
+    def initialize(num)
+      raise unless num.is_a?(Numeric)
+      @x = num
     end
 
-    def create_new_user(name)
-      user = Struct.new(:user_name).new(name)
-      add_user_to_list(user)
-      user_name = get_user_name(user)
-      "Created user with name: #{user_name}"
-    end
-
-
-    def get_user_name(user)
-      user_name = user and user.user_name
-      if user_name.nil? || user_name.strip.empty?
-        raise NoUserError, "No user or empty name"
-      else
-        user_name
-      end
-    end
-
-
-    private
-
-
-    def add_user_to_list(user)
-      @users << user
-    end
+   def add(y)
+     @x + y
+   end
 
   end
 
--- exercises/ruby_exercise2.rb
+++
@@ -1,5 +0,0 @@
-module RubyExercise2
-  def self.replace_vowels(string)
-    string.gsub!("a", "*").gsub!("e", "$")
-  end
-end
